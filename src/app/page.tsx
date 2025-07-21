027666f0b33ae3ea2778539dc7e470da9148ded0
cndrskr <118576150+cndrskr@users.noreply.github.com>
2025-07-22 02:26:59 +0300

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,72 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <Image
+          className={styles.logo}
+          src="/elbrus-logo-anim.png"
+          alt="logo"
+          width={640}
+          height={480}
+          priority
+        />
+        <ol>
+          <li>Тестовая страница проекта развернутая на Vercel.</li>
+          <li>
+            Надо установить либу UI компонентов Material, Ant Design или еще
+            что-то на выбор...
+          </li>
+          <li>еще добавить обработку таблиц эксель и роутинг по страницам</li>
+          <li>еще какую нибудь херню прикрутить...</li>
+        </ol>
+      </main>
+      <footer className={styles.footer}>
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
+    </div>
+  );
+}
